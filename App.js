53395c0adcc02af993f1ddea0c29c6ff40b30e42
jean-cavalcante1 <jjjcts2020@gmail.com>
2025-04-09 12:06:47 -0300

    Adicionando arquivos principais do Manga
    Upload dos arquivos base do projeto NXBank: Aplicativo.js, aplicativo.json e pacote.json. 
     Esses arquivos contêm a lógica inicial, dependências e configurações essenciais do app.

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo ao Manga Bank!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#121212',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+});
